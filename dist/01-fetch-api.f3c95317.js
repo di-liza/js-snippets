6c5c76492c3317b826247f078f51038f037c1e34
Liza_Diachenko01~ <liza.diachenko.99@gmail.com>
2023-03-24 23:05:29 +0200

    add fetch

--- dist/01-fetch-api.f3c95317.js
+++ dist/01-fetch-api.f3c95317.js
@@ -558,6 +558,41 @@ function hmrAccept(bundle, id) {
 
 },{}],"3tpAi":[function(require,module,exports) {
 var _commonCss = require("../css/common.css");
+const pokemonContainer = document.querySelector(".js-card-container");
+const searchForm = document.querySelector(".js-search-form");
+searchForm.addEventListener("submit", onSearchForm);
+function onSearchForm(e) {
+    e.preventDefault();
+    const form = e.currentTarget;
+    const swarchQuery = form.elements.query.value;
+    fetchPokemonById(swarchQuery).then(renderPokemonCard).catch(onError);
+}
+function fetchPokemonById(pokemonId) {
+    return fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}/`).then((r)=>r.json());
+}
+function renderPokemonCard(pokemon) {
+    const markup = `<div class="card">
+    <div class="card-img-top">
+        <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}">
+    </div>
+    <div class="card-body">
+        <h2 class="card-title">Имя: ${pokemon.name}</h2>
+        <p class="card-text">Вес: ${pokemon.weight}</p>
+        <p class="card-text">Рост: ${pokemon.height}</p>
+        <p class="card-text"><b>Умения</b></p>
+        <ul class="list-group"></ul>
+       ${getPokemonAbilityName(pokemon)}
+        </ul>
+    </div>
+</div>`;
+    pokemonContainer.innerHTML = markup;
+}
+function getPokemonAbilityName(pokemon) {
+    return pokemon.abilities.map((ability)=>`<li class ="list-group-item">${ability.ability.name}</li>`).join("");
+}
+function onError(error) {
+    alert("Упс, что то пошло не так");
+}
 
 },{"../css/common.css":"97fCK"}],"97fCK":[function() {},{}]},["hYkko","3tpAi"], "3tpAi", "parcelRequire10fc")
 
--- src/js/01-fetch-api.js
+++ src/js/01-fetch-api.js
@@ -1,1 +1,48 @@
 import '../css/common.css';
+
+const pokemonContainer = document.querySelector('.js-card-container');
+const searchForm = document.querySelector('.js-search-form');
+searchForm.addEventListener('submit', onSearchForm);
+
+function onSearchForm(e) {
+  e.preventDefault();
+
+  const form = e.currentTarget;
+  const swarchQuery = form.elements.query.value;
+
+  fetchPokemonById(swarchQuery).then(renderPokemonCard).catch(onError);
+}
+
+function fetchPokemonById(pokemonId) {
+  return fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}/`).then(r =>
+    r.json(),
+  );
+}
+
+function renderPokemonCard(pokemon) {
+  const markup = `<div class="card">
+    <div class="card-img-top">
+        <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}">
+    </div>
+    <div class="card-body">
+        <h2 class="card-title">Имя: ${pokemon.name}</h2>
+        <p class="card-text">Вес: ${pokemon.weight}</p>
+        <p class="card-text">Рост: ${pokemon.height}</p>
+        <p class="card-text"><b>Умения</b></p>
+        <ul class="list-group"></ul>
+       ${getPokemonAbilityName(pokemon)}
+        </ul>
+    </div>
+</div>`;
+  pokemonContainer.innerHTML = markup;
+}
+
+function getPokemonAbilityName(pokemon) {
+  return pokemon.abilities
+    .map(ability => `<li class ="list-group-item">${ability.ability.name}</li>`)
+    .join('');
+}
+
+function onError(error) {
+  alert('Упс, что то пошло не так');
+}
